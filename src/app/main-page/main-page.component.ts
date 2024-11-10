35952a3877f86fe1e7dfe841feb8a104c6e404f3
Kai Schulz <info@sparrowmedia.de>
2024-11-10 16:10:27 +0100

    footer styling

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
+import { TechStackComponent } from './tech-stack/tech-stack.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [HeroComponent],
+  imports: [HeroComponent, AboutComponent, TechStackComponent, ProjectsComponent, TestimonialsComponent, ContactComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
