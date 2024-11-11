ef828fb62726f58c8e01a3012d45d956d597c88e
Kai Schulz <info@sparrowmedia.de>
2024-11-11 14:11:18 +0100

    created service for skill icons

--- src/app/main-page/tech-stack/tech-stack.component.ts
+++ src/app/main-page/tech-stack/tech-stack.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { SkillsService, Skill } from '../../models/skills.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tech-stack',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tech-stack.component.html',
-  styleUrl: './tech-stack.component.scss'
+  styleUrl: './tech-stack.component.scss',
 })
 export class TechStackComponent {
+  skillDb: Skill[] = [];
 
+  constructor(private skillsService: SkillsService) {
+    this.skillDb = this.skillsService.getSkills();
+    console.log(this.skillDb);
+  }
 }
