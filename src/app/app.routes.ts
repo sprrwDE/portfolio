4ae351f006d8d58191e0b9643df1708540c20e61
Kai Schulz <info@sparrowmedia.de>
2024-11-11 08:35:48 +0100

    styling of hero section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
+import { ImprintComponent } from './legal/imprint/imprint.component';
+import { DataProtectionComponent } from './legal/data-protection/data-protection.component';
 
 export const routes: Routes = [
-    {path: '', component: MainPageComponent}
+    {path: '', component: MainPageComponent},
+    {path: 'imprint', component: ImprintComponent},
+    {path: 'data-protection', component: DataProtectionComponent}
 ];
