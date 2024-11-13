2720fc10538084fc9e6fccdae93c2bdc242bc56d
Kai Schulz <info@sparrowmedia.de>
2024-11-13 14:30:29 +0100

    added switch toggle

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NgToggleModule } from 'ng-toggle-button';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgToggleModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
