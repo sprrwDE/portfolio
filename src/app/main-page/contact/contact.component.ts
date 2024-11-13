29056ebf6099f17f99e0b1810ec14adc23545f4b
Kai Schulz <info@sparrowmedia.de>
2024-11-13 10:40:53 +0100

    added form logic and php file

--- src/app/main-page/contact/contact.component.ts
+++ src/app/main-page/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 
 interface ContactData {
@@ -17,15 +18,43 @@ interface ContactData {
 })
 export class ContactComponent {
 
+  http = inject(HttpClient)
+
   contactData: ContactData = {
     name: '',
     email: '',
     message: '',
   };
 
-  onSubmit(formRef: NgForm) {
-    if(formRef.valid && formRef.submitted) {
-      console.log(this.contactData);
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://kai-schulz.dev/sendMail.php', // hier php datei verlinken
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+      ngForm.resetForm();
     }
   }
 }
