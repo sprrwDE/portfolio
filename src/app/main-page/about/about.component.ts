0719b7ffa2c574067a4faeed1e994705ed20c709
Kai Schulz <info@sparrowmedia.de>
2024-11-15 11:08:37 +0100

    about paragraphs now iterate and translateable

--- src/app/main-page/about/about.component.ts
+++ src/app/main-page/about/about.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
