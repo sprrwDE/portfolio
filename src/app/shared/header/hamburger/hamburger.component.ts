fc6fa0bf195cbe98a81a23c4981e629e2deb8aa0
Kai Schulz <info@sparrowmedia.de>
2024-11-25 10:27:53 +0100

    added some header css and hamburger menu component

---
+++ src/app/shared/header/hamburger/hamburger.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hamburger',
+  standalone: true,
+  imports: [],
+  templateUrl: './hamburger.component.html',
+  styleUrl: './hamburger.component.scss'
+})
+export class HamburgerComponent {
+
+}
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -25,4 +25,8 @@ export class HeaderComponent {
       this.currentLang = 'DE';
     }
   }
+
+  openHamburger() {
+    
+  }
 }
