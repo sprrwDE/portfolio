a9d417cd767f82eace98ea94482f794a1ce82a64
Kai Schulz <info@sparrowmedia.de>
2024-11-25 10:51:33 +0100

    continued with hamburger menu

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { NgToggleModule } from 'ng-toggle-button';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgToggleModule, TranslateModule],
+  imports: [NgToggleModule, TranslateModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   currentLang: string = 'EN';
+  hamburgerOpen: boolean = false;
 
   constructor(private translate: TranslateService) {
     this.translate.setDefaultLang('en'); 
@@ -27,6 +29,10 @@ export class HeaderComponent {
   }
 
   openHamburger() {
-    
+    this.hamburgerOpen = true;
+  }
+
+  closeHamburger() {
+    this.hamburgerOpen = false;
   }
 }
