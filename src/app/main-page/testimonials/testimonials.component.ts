322ea4bbe392873977fb008fa27e3167f47635a4
Kai Schulz <info@sparrowmedia.de>
2024-11-27 14:58:05 +0100

    trying testimonial animation

--- src/app/main-page/testimonials/testimonials.component.ts
+++ src/app/main-page/testimonials/testimonials.component.ts
@@ -17,24 +17,47 @@ export class TestimonialsComponent {
   testimonialDb: Testimonial[] = [];
   selectedItem: number = 1;
   selectedPage: number = 1;
+  animateRight: boolean = false;
+  animateLeft: boolean = false;
+
   constructor(private testimonialService: TestimonialsService) {
     this.testimonialDb = this.testimonialService.getTestimonials();
     console.log(this.testimonialDb);
   }
 
   nextSlide() {
-    this.selectedPage = (this.selectedPage + 1) % this.testimonialDb.length
-    let lastElement = this.testimonialDb.pop(); 
-    if (lastElement) { 
-      this.testimonialDb.unshift(lastElement); 
-    } 
+    this.selectNext();
+    this.animateRight = true;
+    // console.log(this.animateRight);
+    setTimeout(() => {
+      this.animateRight = false;
+      // console.log(this.animateRight);
+    }, 1000);
+  }
+
+  selectNext() {
+    this.selectedPage = (this.selectedPage + 1) % this.testimonialDb.length;
+    let lastElement = this.testimonialDb.pop();
+    if (lastElement) {
+      this.testimonialDb.unshift(lastElement);
+    }
   }
 
   previousSlide() {
-    this.selectedPage = (this.selectedPage - 1 + this.testimonialDb.length) % this.testimonialDb.length
-    let firstElement = this.testimonialDb.shift(); 
-    if(firstElement) {
-      this.testimonialDb.push(firstElement);} 
+    this.selectPrev()
+    this.animateLeft = true;
+    // console.log(this.animateLeft);
+    setTimeout(() => {
+      this.animateLeft = false;
+      // console.log(this.animateLeft);
+    }, 1000);
+  }
 
+  selectPrev() {
+    this.selectedPage = (this.selectedPage - 1 + this.testimonialDb.length) % this.testimonialDb.length;
+    let firstElement = this.testimonialDb.shift();
+    if (firstElement) {
+      this.testimonialDb.push(firstElement);
+    }
   }
 }
